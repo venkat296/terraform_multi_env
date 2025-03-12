c189add6b5506dc081d5294339958782270fa103
venkatadurgarao <venkatadurgaraoy@gmail.com>
2025-03-12 19:26:08 +0530

    multienv

---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "81s-remote-venkat-state-dev"
+key    = "tfvars-dev"
+region = "us-east-1"
+dynamodb_table = "81s-locking-devv"
---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "81s-remote-venkat-state-prod"
+key    = "tfvars-prod"
+region = "us-east-1"
+dynamodb_table = "81s-locking-prodd"
